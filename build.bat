8906596db8755cdea84decddaa771dc446427862
Jon <jondtaylor@gmail.com>
2024-02-29 18:50:55 -0800

    update
    update

--- build.bat
+++ build.bat
@@ -4,9 +4,11 @@
 :: > build.bat exe - will compile and generate a windows exe binary.
 ::
 
-#ant -buildfile buoy.xml
-
-ant -buildfile buoyx.xml
-
-
+::ant -buildfile buoy.xml
+::ant -buildfile buoyx.xml
+::ant -buildfile Renderers.xml
+::ant -buildfile SPManager.xml  # ???
+::ant -buildfile HelpPlugin.xml
+::ant -buildfile Tools.xml
+::ant -buildfile Translators.xml
 ant -buildfile ArmaRender.xml %1
