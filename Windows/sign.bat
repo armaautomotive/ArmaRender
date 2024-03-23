d054afc3b9a90e86a662ef8ff4a33b4d2c355b47
Jon <jondtaylor@gmail.com>
2024-03-23 14:54:28 -0700

    add sign script
    add sign script

---
+++ Windows/sign.bat
@@ -0,0 +1,2 @@
+
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\SignTool.exe" sign /fd SHA256  /a /n "Arma Automotive Inc." /t http://timestamp.digicert.com ./Output/ArmaRender_win_x64_Setup.exe
