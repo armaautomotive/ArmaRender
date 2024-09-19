a6c063c83ac70c3c81a60a2e8fd25b208fa418b1
Jon <jondtaylor@gmail.com>
2024-09-19 13:43:38 -0700

    update

--- ArmaRender/src/armarender/WireframeMesh.java
+++ ArmaRender/src/armarender/WireframeMesh.java
@@ -1,15 +1,7 @@
 /* A WireframeMesh represents an object to be rendered to the screen as a wireframe.  It is
    described by an array of vertices and a list of lines to be drawn betwee them. */
 
-/* Copyright (C) 2000 by Peter Eastman
 
-   This program is free software; you can redistribute it and/or modify it under the
-   terms of the GNU General Public License as published by the Free Software
-   Foundation; either version 2 of the License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
 
 package armarender;
 
